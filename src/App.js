077162a5c20a5793792912d49ef689337a05ebe7
samad86 <mads198644@gmail.com>
2021-01-20 23:54:45 +0300

    Получение товаров асинхронно с помощью AJAX-запросов, отображение товаров из JSON

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components'; // webpack при сборке сначала ищет файл index.js по указанному пути
 import { Home, Cart } from './pages';
 
 function App() {
+  const [doors, setDoors] = useState([]);
+
+  // fetch-запрос на получение данных с фейкового сервера db.json.
+  // Когда запрос был отправлен, переконвертирован в json и ответ от сервера пришел, мы этот json прокидываем в setDoors.
+  // В итоге получаем объект и вытаскиваем из него свойство "doors".
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setDoors(json.doors);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
 
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={doors} />} />
         <Route path="/cart" component={Cart} />
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './Header'; // import и export в 1 стро�
 export { default as Button } from './Button';
 export { default as Categories } from './Categories';
 export { default as SortPopup } from './SortPopup';
+export { default as DoorBlock } from './DoorBlock';
